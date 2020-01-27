37ea65152aa6daba82342b1db4329872b87e6faa
Huginn-Muninn <43746764+Huginn-Muninn@users.noreply.github.com>
2020-01-27 21:12:33 +0000

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 0.2f;
+
+    public GameObject completeLevelUI;
+
+    public GameObject failedLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void FailedLevel()
+    {
+        failedLevelUI.SetActive(true);
+    }
+
+    public void EndGame ()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            // Restart Game
+            FailedLevel();
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
